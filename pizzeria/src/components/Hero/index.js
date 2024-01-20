2430398dc782de2d79225f6178231dbcc020e747
faithwest <faithchepkoech425@gmail.com>
2024-01-21 02:15:41 +0300

    created sidebar,navbar and hero components for the web app

---
+++ pizzeria/src/components/Hero/index.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Navbar from '../Navbar';
+import  { HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn } from './HeroElements';
+import Sidebar from '../Sidebar';
+
+
+const Hero = () => {
+    const  [isOpen, setIsOpen] = useState(false);
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    };
+    return (
+      <HeroContainer>
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <HeroContent>
+        <HeroItems>
+                <HeroH1>Greatest Pizza</HeroH1>
+                <HeroP> Ready in Seconds</HeroP>
+                <HeroBtn>Place Order</HeroBtn>
+        </HeroItems>
+      </HeroContent>
+      </HeroContainer>
+    )
+  }
+  
+  export default Hero;
--- pizzeria/src/components/Navbar/NavbarElements.js
+++ pizzeria/src/components/Navbar/NavbarElements.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 import { NavLink as Link } from 'react-router-dom';
+import { FaPizzaSlice } from 'react-icons/fa';
 
 export const Nav = styled.nav`
-  background: orange;
-  height:'80px;
+  background: transparent;
+  height: 80px;
   display: flex;
   justify-content:center;
   font-weight: 700;
@@ -17,7 +18,7 @@ export const NavLink = styled(Link)`
     text-decoration: none;
     cursor: pointer;
 
-    @media screen and (max-width: 400px) {
+@media screen and (max-width: 400px) {
         position: absolute;
         top: 10px;
         left: 25px;
@@ -25,14 +26,24 @@ export const NavLink = styled(Link)`
 
     `;
 
- export const NavIcon = styled.div`
+ export const NavIcon = styled.div` 
     display : block;
-    position: ablolute;
+    position: absolute;
     top: 0;
     right: 0;
     cursor: pointer;
     color: #fff;
 
-    `;   
+    p {
+        transform: translate(-175%, 100%);
+        font-weight: bold;
+    }
+
+    ` 
+
+ export const Bars = styled(FaPizzaSlice)`
+    font-size: 2rem;
+    transform: translate(-50%, -15%);
+ `;
   
   
